aba3ca569027ec1dd46bc6884198662e0c8dfed1
simone <simon387@hotmail.it>
2021-12-14 00:00:25 +0100

    spaces

--- index.php
+++ index.php
@@ -19,18 +19,13 @@
 	<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 	<link href="assets/css/style.css" rel="stylesheet">
 </head>
-
 <body>
-
-<!-- ======= Header ======= -->
 <header id="header">
 	<div class="container">
-
 		<h1><a href="index.php">Zalex</a></h1>
 		<!-- Uncomment below if you prefer to use an image logo -->
 		<!-- <a href="index.php" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
 		<h2>I'm a passionate <span>singer</span> from Italy</h2>
-
 		<nav id="navbar" class="navbar">
 			<ul>
 				<li><a class="nav-link active" href="#header">Home</a></li>
@@ -42,28 +37,21 @@
 			</ul>
 			<i class="bi bi-list mobile-nav-toggle"></i>
 		</nav>
-
 		<div class="social-links">
 			<a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
 			<a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
 			<a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
 			<a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
 		</div>
-
 	</div>
 </header>
 
-<!-- ======= About Section ======= -->
 <section id="about" class="about">
-
-	<!-- ======= About Me ======= -->
 	<div class="about-me container">
-
 		<div class="section-title">
 			<h2>About</h2>
 			<p>Learn more about me</p>
 		</div>
-
 		<div class="row">
 			<div class="col-lg-4" data-aos="fade-right">
 				<img src="assets/img/me.jpg" class="img-fluid" alt="">
@@ -100,14 +88,10 @@
 				</p>
 			</div>
 		</div>
+	</div>
 
-	</div><!-- End About Me -->
-
-	<!-- ======= Counts ======= -->
 	<div class="counts container">
-
 		<div class="row">
-
 			<div class="col-lg-3 col-md-6">
 				<div class="count-box">
 					<i class="bi bi-emoji-smile"></i>
@@ -116,7 +100,6 @@
 					<p>random counter</p>
 				</div>
 			</div>
-
 			<div class="col-lg-3 col-md-6 mt-5 mt-md-0">
 				<div class="count-box">
 					<i class="bi bi-journal-richtext"></i>
@@ -124,7 +107,6 @@
 					<p>Projects</p>
 				</div>
 			</div>
-
 			<div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
 				<div class="count-box">
 					<i class="bi bi-headset"></i>
@@ -132,7 +114,6 @@
 					<p>random counter</p>
 				</div>
 			</div>
-
 			<div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
 				<div class="count-box">
 					<i class="bi bi-award"></i>
@@ -141,22 +122,15 @@
 					<p>Awards</p>
 				</div>
 			</div>
-
 		</div>
+	</div>
 
-	</div><!-- End Counts -->
-
-	<!-- ======= Skills  ======= -->
 	<div class="skills container">
-
 		<div class="section-title">
 			<h2>Skills</h2>
 		</div>
-
 		<div class="row skills-content">
-
 			<div class="col-lg-6">
-
 				<div class="progress">
 					<span class="skill">HTML <i class="val">100%</i></span>
 					<div class="progress-bar-wrap">
@@ -164,59 +138,46 @@
 						     aria-valuemax="100"></div>
 					</div>
 				</div>
-
 				<div class="progress">
 					<span class="skill">CSS <i class="val">90%</i></span>
 					<div class="progress-bar-wrap">
 						<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
 					</div>
 				</div>
-
 				<div class="progress">
 					<span class="skill">JavaScript <i class="val">75%</i></span>
 					<div class="progress-bar-wrap">
 						<div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
 					</div>
 				</div>
-
 			</div>
-
 			<div class="col-lg-6">
-
 				<div class="progress">
 					<span class="skill">PHP <i class="val">80%</i></span>
 					<div class="progress-bar-wrap">
 						<div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
 					</div>
 				</div>
-
 				<div class="progress">
 					<span class="skill">WordPress/CMS <i class="val">90%</i></span>
 					<div class="progress-bar-wrap">
 						<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
 					</div>
 				</div>
-
 				<div class="progress">
 					<span class="skill">Photoshop <i class="val">55%</i></span>
 					<div class="progress-bar-wrap">
 						<div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
 					</div>
 				</div>
-
 			</div>
-
 		</div>
+	</div>
 
-	</div><!-- End Skills -->
-
-	<!-- ======= Interests ======= -->
 	<div class="interests container">
-
 		<div class="section-title">
 			<h2>Interests</h2>
 		</div>
-
 		<div class="row">
 			<div class="col-lg-3 col-md-4">
 				<div class="icon-box">
@@ -291,19 +252,14 @@
 				</div>
 			</div>
 		</div>
+	</div>
 
-	</div><!-- End Interests -->
-
-	<!-- ======= Testimonials ======= -->
 	<div class="testimonials container">
-
 		<div class="section-title">
 			<h2>Testimonials</h2>
 		</div>
-
 		<div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
 			<div class="swiper-wrapper">
-
 				<div class="swiper-slide">
 					<div class="testimonial-item">
 						<p>
@@ -316,8 +272,7 @@
 						<h3>Saul Goodman</h3>
 						<h4>Ceo &amp; Founder</h4>
 					</div>
-				</div><!-- End testimonial item -->
-
+				</div>
 				<div class="swiper-slide">
 					<div class="testimonial-item">
 						<p>
@@ -330,8 +285,7 @@
 						<h3>Sara Wilsson</h3>
 						<h4>Designer</h4>
 					</div>
-				</div><!-- End testimonial item -->
-
+				</div>
 				<div class="swiper-slide">
 					<div class="testimonial-item">
 						<p>
@@ -344,8 +298,7 @@
 						<h3>Jena Karlis</h3>
 						<h4>Store Owner</h4>
 					</div>
-				</div><!-- End testimonial item -->
-
+				</div>
 				<div class="swiper-slide">
 					<div class="testimonial-item">
 						<p>
@@ -358,8 +311,7 @@
 						<h3>Matt Brandon</h3>
 						<h4>Freelancer</h4>
 					</div>
-				</div><!-- End testimonial item -->
-
+				</div>
 				<div class="swiper-slide">
 					<div class="testimonial-item">
 						<p>
@@ -372,29 +324,21 @@
 						<h3>John Larson</h3>
 						<h4>Entrepreneur</h4>
 					</div>
-				</div><!-- End testimonial item -->
-
+				</div>
 			</div>
 			<div class="swiper-pagination"></div>
 		</div>
-
 		<div class="owl-carousel testimonials-carousel">
-
 		</div>
+	</div>
+</section>
 
-	</div><!-- End Testimonials  -->
-
-</section><!-- End About Section -->
-
-<!-- ======= Resume Section ======= -->
 <section id="resume" class="resume">
 	<div class="container">
-
 		<div class="section-title">
 			<h2>Resume</h2>
 			<p>Check My Resume</p>
 		</div>
-
 		<div class="row">
 			<div class="col-lg-6">
 				<h3 class="resume-title">Sumary</h3>
@@ -411,7 +355,6 @@
 					</ul>
 					</p>
 				</div>
-
 				<h3 class="resume-title">Education</h3>
 				<div class="resume-item">
 					<h4>Master of Fine Arts &amp; Graphic Design</h4>
@@ -465,19 +408,15 @@
 				</div>
 			</div>
 		</div>
-
 	</div>
-</section><!-- End Resume Section -->
+</section>
 
-<!-- ======= Services Section ======= -->
 <section id="services" class="services">
 	<div class="container">
-
 		<div class="section-title">
 			<h2>Services</h2>
 			<p>My Services</p>
 		</div>
-
 		<div class="row">
 			<div class="col-lg-4 col-md-6 d-flex align-items-stretch">
 				<div class="icon-box">
@@ -486,7 +425,6 @@
 					<p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
 				<div class="icon-box">
 					<div class="icon"><i class="bx bx-file"></i></div>
@@ -494,7 +432,6 @@
 					<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
 				<div class="icon-box">
 					<div class="icon"><i class="bx bx-tachometer"></i></div>
@@ -502,7 +439,6 @@
 					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
 				<div class="icon-box">
 					<div class="icon"><i class="bx bx-world"></i></div>
@@ -510,7 +446,6 @@
 					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
 				<div class="icon-box">
 					<div class="icon"><i class="bx bx-slideshow"></i></div>
@@ -518,7 +453,6 @@
 					<p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
 				<div class="icon-box">
 					<div class="icon"><i class="bx bx-arch"></i></div>
@@ -526,21 +460,16 @@
 					<p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
 				</div>
 			</div>
-
 		</div>
-
 	</div>
-</section><!-- End Services Section -->
+</section>
 
-<!-- ======= Portfolio Section ======= -->
 <section id="portfolio" class="portfolio">
 	<div class="container">
-
 		<div class="section-title">
 			<h2>Portfolio</h2>
 			<p>My Works</p>
 		</div>
-
 		<div class="row">
 			<div class="col-lg-12 d-flex justify-content-center">
 				<ul id="portfolio-flters">
@@ -551,9 +480,7 @@
 				</ul>
 			</div>
 		</div>
-
 		<div class="row portfolio-container">
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-app">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
@@ -569,7 +496,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-web">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
@@ -585,7 +511,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-app">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
@@ -601,7 +526,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-card">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
@@ -617,7 +541,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-web">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
@@ -633,7 +556,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-app">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
@@ -649,7 +571,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-card">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
@@ -665,7 +586,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-card">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
@@ -681,7 +601,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-lg-4 col-md-6 portfolio-item filter-web">
 				<div class="portfolio-wrap">
 					<img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
@@ -697,23 +616,17 @@
 					</div>
 				</div>
 			</div>
-
 		</div>
-
 	</div>
-</section><!-- End Portfolio Section -->
+</section>
 
-<!-- ======= Contact Section ======= -->
 <section id="contact" class="contact">
 	<div class="container">
-
 		<div class="section-title">
 			<h2>Contact</h2>
 			<p>Contact Me</p>
 		</div>
-
 		<div class="row mt-2">
-
 			<div class="col-md-6 d-flex align-items-stretch">
 				<div class="info-box">
 					<i class="bx bx-map"></i>
@@ -721,7 +634,6 @@
 					<p>A108 Adam Street, New York, NY 535022</p>
 				</div>
 			</div>
-
 			<div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
 				<div class="info-box">
 					<i class="bx bx-share-alt"></i>
@@ -735,7 +647,6 @@
 					</div>
 				</div>
 			</div>
-
 			<div class="col-md-6 mt-4 d-flex align-items-stretch">
 				<div class="info-box">
 					<i class="bx bx-envelope"></i>
@@ -751,7 +662,6 @@
 				</div>
 			</div>
 		</div>
-
 		<form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
 			<div class="row">
 				<div class="col-md-6 form-group">
@@ -776,9 +686,8 @@
 				<button type="submit">Send Message</button>
 			</div>
 		</form>
-
 	</div>
-</section><!-- End Contact Section -->
+</section>
 
 <div class="credits">
 	<!-- All the links in the footer should remain intact. -->
@@ -788,7 +697,6 @@
 	Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
 </div>
 
-<!-- Vendor JS Files -->
 <script src="assets/vendor/purecounter/purecounter.js"></script>
 <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
@@ -796,10 +704,6 @@
 <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
 <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
 <script src="assets/vendor/php-email-form/validate.js"></script>
-
-<!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
-
 </body>
-
 </html>
