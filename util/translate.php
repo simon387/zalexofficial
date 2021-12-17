bb70a2c858013e335e184c20ae7023f2a26c2e71
simone <simon387@hotmail.it>
2021-12-17 19:04:27 +0100

    space

--- util/translate.php
+++ util/translate.php
@@ -20,20 +20,19 @@ function tr($key)
 		$index = 3;
 	}
 	$matrix = array(//key, en, es, it
-		array("i_am", "I'm a passionate ", "Soy una ", "Sono una "),
-		array("singer", "singer", "cantante", "cantante"),
-		array("from_italy", " from Italy", " italiana", " italiana"),
-		array("home", "Home", "Inicio", "Home"),
-		array("about", "About", "Acerca de mí", "Info"),
-		array("my_music", "My music", "La mi musica", "La mia musica"),
-		array("my_music_profiles", "My music profiles", "Mis perfiles musicales", "I miei profili musicali
-"),
-		array("my_media", "My Media", "Mis medias", "I miei media"),
-		array("all", "All", "Todos", "Tutti"),
-		array("photos", "Photos", "Fotos", "Foto"),
-		array("contact", "Contact", "Contactos", "Contatti"),
-		array("contact_me", "Contact Me", "Contáctame", "Contattami"),
-		array("learn_more", "Learn more about me", "Aprende más sobre mi", "Scopri di più su di me"),
+			array("i_am", "I'm a passionate ", "Soy una ", "Sono una "),
+			array("singer", "singer", "cantante", "cantante"),
+			array("from_italy", " from Italy", " italiana", " italiana"),
+			array("home", "Home", "Inicio", "Home"),
+			array("about", "About", "Acerca de mí", "Info"),
+			array("my_music", "My music", "La mi musica", "La mia musica"),
+			array("my_music_profiles", "My music profiles", "Mis perfiles musicales", "I miei profili musicali"),
+			array("my_media", "My Media", "Mis medias", "I miei media"),
+			array("all", "All", "Todos", "Tutti"),
+			array("photos", "Photos", "Fotos", "Foto"),
+			array("contact", "Contact", "Contactos", "Contatti"),
+			array("contact_me", "Contact Me", "Contáctame", "Contattami"),
+			array("learn_more", "Learn more about me", "Aprende más sobre mi", "Scopri di più su di me"),
 	);
 	for ($i = 0; $i < count($matrix); $i++) {
 		if ($matrix[$i][0] == $key) {
